1291010c316b068f496a8844df8d2d534aa1c184
David Portilla <d.portilla@outlook.com>
2015-02-09 16:51:54 +0100

    tetris.adb cleaned

--- tetris.adb
+++ tetris.adb
@@ -1,5 +1,5 @@
 with Ada.Real_Time; use Ada.Real_Time;
-with Screen, Bricks, Wall, Game_Avance, Text_IO;
+with Screen, Bricks, Wall, Game_Avance, User_Interaction, Text_IO;
 
 procedure Tetris is
    pragma Priority (4);
@@ -16,20 +16,5 @@ procedure Tetris is
       Text_IO.Put_Line (" TETRIS Ada ");
       Screen.MoveCursor ((Column => 1, Row => 5));
       Text_IO.Put_Line ("2=drop 4=left 5=spin 6=right");
-
-      --loop
-      --  Text_IO.Get_Immediate (user_input, available);
-      --  exit when available;
-      --  T := T + milliseconds(10);
-      --  delay until T;
-    --end loop;
-      -- 2 down arrow
-      -- 4 left arrow
-      -- 6 right arrow
-      -- 5 rotate
-      -- Y exit
-
-
-   --Screen.ClearScreen;
 
 end Tetris;
