42ef863594b30e8237c75b161145b2c4e13ae7ed
David Portilla <d.portilla@outlook.com>
2015-02-09 16:50:14 +0100

    Fixed barrier in Shared_Actions

--- actions.adb
+++ actions.adb
@@ -5,11 +5,13 @@ package body Actions is
     procedure SetAction(X : in ActionMode) is
     begin
       action := X;
+      flag := true;
     end SetAction;
 
     entry GetAction (X : out ActionMode) when flag is
     begin
       X := action;
+      flag := false;
     end GetAction;
 
   end Shared_Action;
